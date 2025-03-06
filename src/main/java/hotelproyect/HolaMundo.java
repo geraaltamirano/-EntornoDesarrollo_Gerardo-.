bc3add3a5f42744ef533c6af5ce9ebb36b7923e5
geraaltamirano <geraaltaglz0921@gmail.com>
2025-03-06 01:08:41 -0600

    tercer commit

--- src/main/java/hotelproyect/HolaMundo.java
+++ src/main/java/hotelproyect/HolaMundo.java
@@ -2,6 +2,6 @@ package hotelproyect;
 
 public class HolaMundo {
 	public static void main (String[] args) {
-	    System.out.println("Hola, mundo!");
+	    System.out.println("Hola, ITT3!");
 	  }
 }
